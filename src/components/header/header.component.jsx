add02f9c2f024e99d1227931344073438535b570
Mateen Kazia <mateencode@gmail.com>
2020-02-08 19:48:56 -0600

    setting up styled components

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ReactComponent as Logo } from "../../assests/logo.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Header;
