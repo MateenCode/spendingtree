4611466a919a5fc61791e328ebfa8f79fee9c885
Mateen Kazia <mateencode@gmail.com>
2020-02-08 19:53:00 -0600

    setting up homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Header from "../../components/header/header.component";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default HomePage;
